007069f394ebc143dee544ed96f8578f23a5586b
Ryan Berliner <ryan.berliner@gmail.com>
2023-10-08 09:49:56 -0400

    support for browsers without conccurency

---
+++ search-utils.js
@@ -0,0 +1,107 @@
+import episodes from 'the-office';
+
+const lines = [];
+
+export function cleanLine(line) {
+  return line.replace(/[^a-zA-Z0-9 ]/g, '').toLowerCase();
+}
+
+function compareIds(a, b) {
+  const [ae, as, al] = a.split('-').map(x => parseInt(x));
+  const [be, bs, bl] = b.split('-').map(x => parseInt(x));
+  if (ae < be) return -1;
+  if (be < ae) return 1;
+  if (as < bs) return -1;
+  if (bs < as) return 1;
+  if (al < bl) return -1;
+  if (bl < al) return 1;
+  return 0;
+}
+
+function idIntersection(a, b) {
+  let ai = 0;
+  let bi = 0;
+  const inter = [];
+  while (ai < a.length && bi < b.length) {
+    const comp = compareIds(a[ai], b[bi]);
+    if (comp === 0) {
+      inter.push(a[ai]);
+      ai++;
+      bi++;
+    } else if (comp > 0) { bi++; } else { ai++; }
+  }
+  return inter;
+}
+
+episodes.forEach((episode, e) => {
+  episode.scenes.forEach((scene, s) => {
+    scene.forEach((line, l) => {
+      lines.push({
+        line: cleanLine(line.line),
+        id: `${e}-${s}-${l}`
+      });
+    });
+  });
+});
+
+const trie = {'rec': [], 'desc': {}};
+
+function ingest(word, id, triePart) {
+  if (word.length === 0) return;
+  const c = word[0];
+  if (!triePart[c]) triePart[c] = {'rec': [], 'desc': {}};
+  if (word.length === 1) triePart[c]['rec'].push(id);
+  return ingest(word.substring(1), id, triePart[c]['desc'])
+}
+
+function retrieveAll(tree) {
+  const direct = tree?.rec || [];
+  const children = [];
+  Object.keys(tree?.desc || {}).forEach(key => {
+    if (key === 'rec') return;
+    children.push(...retrieveAll(tree.desc[key]));
+  });
+  return [...direct, ...children];
+}
+
+lines.forEach(line => {
+  const words = new Set([...line.line.split(' ')]);
+  words.forEach(word => {
+    ingest(word, line.id, trie['desc']);
+  });
+});
+
+export function retrieveResults(text) {
+  const cleaned = cleanLine(text);
+  const words = new Set([...cleaned.split(' ').filter(word => !!word)]);
+  if (!words) return;
+
+  const lineIds = [];
+
+  words.forEach(word => {
+    let tree = trie;
+    word.split('').forEach(c => {
+      tree = tree?.['desc']?.[c];
+    });
+    lineIds.push(retrieveAll(tree).sort(compareIds));
+  });
+
+  // lets sort by the most exclusive term first
+  lineIds.sort((a, b) => a.length < b.length ? -1 : a.length > b.length ? 1 : 0);
+
+  const intersection = lineIds.reduce((prev, curr) => {
+    if (prev === false) return Array.from(new Set(curr));
+    return idIntersection(prev, curr);
+  }, false);
+
+  // lets assume short quotes are a closer match
+  (intersection || []).sort((a, b) => {
+    const [ae, as, al] = a.split('-').map(x => parseInt(x));
+    const [be, bs, bl] = b.split('-').map(x => parseInt(x));
+    const linea = episodes[ae].scenes[as][al].line;
+    const lineb = episodes[be].scenes[bs][bl].line;
+    return linea.length < lineb.length ? -1 : linea.length > lineb.length ? 1 : 0;
+  });
+
+  return intersection;
+}
--- search.js
+++ search.js
@@ -1,4 +1,5 @@
 import episodes from 'the-office';
+import { cleanLine, retrieveResults } from './search-utils';
 
 const search = document.querySelector('[type="search"]');
 const results = document.getElementById('results');
@@ -14,49 +15,59 @@ if (window.Worker) {
 
 let currentWorker = 0;
 let searchTimeout = null;
-const debounce = 50;
-search.addEventListener('input', event => {
+const debounce = searchWorkers.length ? 50 : 1000;
+function performSearch(value) {
   clearTimeout(searchTimeout);
   searchTimeout = setTimeout(() => {
-    searchWorkers[currentWorker].postMessage([performance.now(), event.target.value]);
-    currentWorker = currentWorker < searchWorkers.length  - 1 ? currentWorker + 1 : 0;
+    if (searchWorkers.length) {
+      searchWorkers[currentWorker].postMessage([performance.now(), value]);
+      currentWorker = currentWorker < searchWorkers.length  - 1 ? currentWorker + 1 : 0;
+    } else {
+      showResults(retrieveResults(value));
+    }
   }, debounce);
-});
-
-function cleanLine(line) {
-  return line.replace(/[^a-zA-Z0-9 ]/g, '').toLowerCase();
 }
+
+function showResults(ids) {
+  results.innerHTML = '';
+  const reg = new RegExp('\\b' + search.value.split(' ').map(x => cleanLine(x).split('').join('[^a-zA-Z0-9 ]?')).filter(x => !!x).join('|\\b'), 'gi');
+  for (let i = 0; i < 100; i++) {
+    const inter = ids[i];
+    if (!inter) return;
+    const [e, s, l] = inter.split('-');
+    const line = episodes[parseInt(e)].scenes[parseInt(s)][l];
+    const quote = document.createElement('li');
+    let adjusted = line.line.replace(reg, "|M|$&|M|");
+    adjusted = adjusted.replaceAll("|M| |M|", " ");
+    adjusted.split("|M|").forEach((part, i) => {
+      let node;
+      if (i % 2) {
+        node = document.createElement('mark');
+        node.innerHTML = part;
+      } else {
+        node = document.createTextNode(part);
+      }
+      quote.appendChild(node);
+    });
+    results.append(quote);
+  };
+}
+
+search.addEventListener('input', event => {
+  performSearch(event.target.value);
+});
 
 // theres gotta be a way to delegate this...
 let lastUpdate = performance.now();
 searchWorkers.forEach(worker => {
   worker.onmessage = e => {
-    const [time, intersection, data] = e.data;
+    const [time, ids, data] = e.data;
     if (time < lastUpdate) return;
     console.log(data);
     lastUpdate = time;
-    let count = 0;
-    results.innerHTML = '';
-    const reg = new RegExp('\\b' + search.value.split(' ').map(x => cleanLine(x).split('').join('[^a-zA-Z0-9 ]?')).filter(x => !!x).join('|\\b'), 'gi');
-    for (let i = 0; i < 100; i++) {
-      const inter = intersection[i];
-      if (!inter) return;
-      const [e, s, l] = inter.split('-');
-      const line = episodes[parseInt(e)].scenes[parseInt(s)][l];
-      const quote = document.createElement('li');
-      let adjusted = line.line.replace(reg, "|M|$&|M|");
-      adjusted = adjusted.replaceAll("|M| |M|", " ");
-      adjusted.split("|M|").forEach((part, i) => {
-        let node;
-        if (i % 2) {
-          node = document.createElement('mark');
-          node.innerHTML = part;
-        } else {
-          node = document.createTextNode(part);
-        }
-        quote.appendChild(node);
-      });
-      results.append(quote);
-    };
+    showResults(ids);
   };
 });
+
+// on load
+performSearch(search.value);
