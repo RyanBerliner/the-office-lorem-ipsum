4e006a6517ba5ca135efaf32d96d31399776aa84
Ryan Berliner <22206986+RyanBerliner@users.noreply.github.com>
2024-07-27 09:12:23 -0400

    add episode explorer to the search page (#1)
    * remove dead code
    
    * prototype
    
    * jump to quote, style a bit better
    
    * styling tweaks
    
    * use link for keyboard access and browser styling
    
    * styling tweaks
    
    * remove console log

--- search.js
+++ search.js
@@ -3,6 +3,7 @@ import { cleanLine, Search } from './search-utils';
 
 const search = document.querySelector('[type="search"]');
 const results = document.getElementById('results');
+const episodeExplorer = document.getElementById('episode-explorer');
 const searchObj = new Search();
 
 // lets boot as many workers at there are cores, cycle through them on
@@ -62,7 +63,11 @@ function showResults(ids) {
       quote.appendChild(node);
     });
     const wrapper = document.createElement('li');
-    wrapper.appendChild(quote);
+    const link = document.createElement('a');
+    link.setAttribute('href', '#episode-explorer');
+    link.setAttribute('data-quote', inter);
+    link.appendChild(quote);
+    wrapper.appendChild(link);
     const info = document.createElement('span');
     info.innerHTML = `<strong>${line.character}</strong> in Season ${episode.season} Episode ${episode.episode} "${episode.title}"`;
     wrapper.appendChild(info);
@@ -74,13 +79,55 @@ search.addEventListener('input', event => {
   performSearch(event.target.value);
 });
 
+results.addEventListener('click', event => {
+  const target = event.target;
+  const quote = event.target.closest('[data-quote]');
+  if (!quote) return;
+  const [e, s, l] = quote.dataset.quote.split('-');
+  const episode = episodes[parseInt(e)]
+  const line = episode.scenes[parseInt(s)][l];
+  const ul = document.createElement('ul');
+  episode.scenes.forEach((scene, s) => {
+    scene.forEach((line, l) => {
+      const li = document.createElement('li');
+      li.setAttribute('data-quote', `${e}-${s}-${l}`)
+      const p = document.createElement('p');
+      const sp = document.createElement('span');
+      sp.innerHTML = line.line;
+      p.appendChild(sp);
+      li.appendChild(p);
+      const c = document.createElement('span');
+      c.classList.add('character');
+      c.innerHTML = line.character;
+      li.appendChild(c);
+      ul.appendChild(li);
+    });
+  });
+  episodeExplorer.innerHTML = '';
+  const heading = document.createElement('h3');
+  heading.innerHTML = `Season ${episode.season} Episode ${episode.episode} "${episode.title}"`;
+  const close = document.createElement('button');
+  close.innerHTML = 'Close Episode';
+  episodeExplorer.appendChild(heading);
+  episodeExplorer.appendChild(ul);
+  episodeExplorer.appendChild(close);
+  const interest = episodeExplorer.querySelector(`[data-quote="${quote.dataset.quote}"]`);
+  interest.classList.add('selected-quote');
+  episodeExplorer.scrollTop = interest.offsetTop - (episodeExplorer.offsetHeight/2) + (interest.offsetHeight/2);
+});
+
+episodeExplorer.addEventListener('click', event => {
+  const button = event.target.closest('button');
+  if (!button) return;
+  episodeExplorer.innerHTML = '';
+});
+
 // theres gotta be a way to delegate this...
 let lastUpdate = performance.now();
 searchWorkers.forEach(worker => {
   worker.onmessage = e => {
     const [time, ids, data] = e.data;
     if (time < lastUpdate) return;
-    // console.log(data);
     lastUpdate = time;
     showResults(ids);
   };
